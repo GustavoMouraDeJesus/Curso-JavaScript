a6b74b5f0ff9ae30c982195a27cd7747fd3b5675
Gustavo Moura <gustavomouradejesus@gmail.com>
2025-08-28 00:23:22 -0300

    Aula 1 e 2 realizadas com sucesso

---
+++ AULA2/JS/aula2.js
@@ -0,0 +1,23 @@
+function adicionarTarefa() { 
+      //A primeira linha se trata de uma função, qunadoa  função é chamada, todo nosso código a baixo é executado em ordem.
+      let mensagem = "Tarefa adicionada com sucesso!";
+
+      let inputTarefa = document.getElementById("inputTarefa")
+      //A primeira coisa que acontece é que estamos buscando no nosso documento HTML um elemento com o id inputTarefa, que no caso é o nosso input mesmo.
+      let tarefa = inputTarefa.value
+      //Pegamos o elemento do HTMl input e colocamos dentro da variavel a cima, pegamos esse inputTarefa e pegamos o valor dele input, ou seja, tudo oque o usuario digitar dentro do inputTarefa
+      document.getElementById ("mensagem").textContent = mensagem;
+
+      let listaTarefas = document.getElementById("listaTarefas")
+      //Estamos pegando tambem no nosso HTMl o elemento UL com o getElementById("listaTarefas"), e agente coloca dentro de uma variavel, pelo fato de querer inserir LI's dentro dessa variavel
+      let novaTarefa = document.createElement("li")
+      //Aqui seria a criação dos li com o createElement e colocamos esse elemento dentro de uma variavel
+
+      novaTarefa.textContent = tarefa
+      //Aqui estamos colocando para nosso elemento li o valor da nossa tarefa, ou seja, do nosso input. Ele vai pegar o valor da tarefa lá em cima na variavel tarefa.
+
+      listaTarefas.appendChild(novaTarefa)
+      //Por fim a gente pega o elemento ul e damos um appendChild, adiciona um elemento filho, que seria para o usuario poder adicionar uma nova tarefa. 
+
+      inputTarefa.value = ""
+}
