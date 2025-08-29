3a08405132379474d5ed6eca79b6b4eb4daba2f7
Gustavo Moura <gustavomouradejesus@gmail.com>
2025-08-28 22:44:45 -0300

    aula 3

--- AULA2/JS/aula2.js
+++ AULA2/JS/aula2.js
@@ -1,11 +1,16 @@
 function adicionarTarefa() { 
-      //A primeira linha se trata de uma função, qunadoa  função é chamada, todo nosso código a baixo é executado em ordem.
+            //A primeira linha se trata de uma função, qunadoa  função é chamada, todo nosso código a baixo é executado em ordem.
       let mensagem = "Tarefa adicionada com sucesso!";
 
       let inputTarefa = document.getElementById("inputTarefa")
       //A primeira coisa que acontece é que estamos buscando no nosso documento HTML um elemento com o id inputTarefa, que no caso é o nosso input mesmo.
       let tarefa = inputTarefa.value
       //Pegamos o elemento do HTMl input e colocamos dentro da variavel a cima, pegamos esse inputTarefa e pegamos o valor dele input, ou seja, tudo oque o usuario digitar dentro do inputTarefa
+      if(tarefa === "") {
+            alert("Adicone uma tarefa");
+            return; //interronpe a operação se tiver vazio
+      }
+      else{
       document.getElementById ("mensagem").textContent = mensagem;
 
       let listaTarefas = document.getElementById("listaTarefas")
@@ -20,4 +25,9 @@ function adicionarTarefa() {
       //Por fim a gente pega o elemento ul e damos um appendChild, adiciona um elemento filho, que seria para o usuario poder adicionar uma nova tarefa. 
 
       inputTarefa.value = ""
+      }
 }
+
+//O if quer dizer que "se" a tarefa for igual a (campo vazio) ele retorna uma mensagem para o usuario dizendo que é necessario adicionar uma tarefa, e o return serve para voltar ao começo do código.
+
+//Tem o Else tambem que significa que (senão) estiver vazio, ele executa o comando que esta dentro de si.
